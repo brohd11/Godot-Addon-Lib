579e5d91918353c9287f36057f54a16634aa98c0
brohd <brohdie_larocque@hotmail.com>
2026-04-26 05:20:18 -0700

    update git checking

--- git/git_helper.gd
+++ git/git_helper.gd
@@ -1,13 +1,58 @@
 class_name Git
 
-static func check_addons_git():
-	for folder in DirAccess.get_directories_at("res://addons"):
-		var path = "res://addons".path_join(folder)
-		var git_path = path.path_join(".git")
-		if FileAccess.file_exists(git_path) or DirAccess.dir_exists_absolute(git_path):
-			var dirty = get_git_status(path)
-			if dirty:
-				print("Uncommmited changes in: %s" % path)
+const OPEN_IN_TERM_SH = "res://tools/git_submodule/open_in_term.sh"
+const GET_SUBMODULES_SH = "res://tools/git_submodule/get_submodules_bundled.sh"
+
+static func list_submodules_sh(arg:=""):
+	var args = []
+	args.append("--raw")
+	if arg != "":
+		args.append(arg)
+	
+	var sh_path = ProjectSettings.globalize_path(GET_SUBMODULES_SH)
+	
+	var output = []
+	var exit_code = OS.execute(sh_path, args, output)
+	if exit_code == -1:
+		printerr("Could not get submodules.")
+		return
+	
+	print("CODE::", exit_code, "::", output)
+	
+	pass
+
+static func list_submodules(only_dirty:=false):
+	var args = ["submodule", "status"]
+	var output = []
+	var exit_code = OS.execute("git", args, output)
+	if exit_code == -1:
+		printerr("Could not get submodules.")
+		return
+	var submod_str = output[0] as String
+	var submodules_strings = submod_str.strip_edges().split("\n", false)
+	var submodule_paths = []
+	for string in submodules_strings:
+		var path = string.strip_edges().split(" ")[1]
+		if not path.is_empty():
+			submodule_paths.append(path)
+	
+	var to_show = []
+	if only_dirty:
+		print("=== Dirty Submodules ===")
+		for path in submodule_paths:
+			if get_git_status(path):
+				to_show.append(path)
+	else:
+		print("=== All Submodules ===")
+		to_show = submodule_paths
+	
+	
+	if to_show.is_empty():
+		print("(None to show)")
+	else:
+		for p in to_show:
+			print(" - ", p)
+
 
 static func get_git_status(dir):
 	var args = [
@@ -27,3 +72,22 @@ static func get_git_status(dir):
 		return false
 	elif exit_code == 1: #dirty
 		return true
+
+
+static func open_submodules_in_term(only_dirty:=false):
+	if not FileAccess.file_exists(OPEN_IN_TERM_SH):
+		print("No open in term script present.")
+		return
+	
+	var args = []
+	if only_dirty:
+		args.append("-d")
+	
+	var sh_path = ProjectSettings.globalize_path(OPEN_IN_TERM_SH)
+	
+	var output = []
+	var exit_code = OS.execute(sh_path, args, output)
+	if exit_code == -1:
+		printerr("Could not run script.")
+		return
+	print(exit_code)
